7de101054bb36fd35142025143ecad991df283c1
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-06 11:37:35 -0700

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,23 +6,28 @@ contents of the web_static folder of your AirBnB Clone repo.
 
 import os.path
 from datetime import datetime
-from fabric.api import local
+from fabric.api import local, runs_once
 
 
+@runs_once
 def do_pack():
-    """Archives the static files."""
-    cur_time = datetime.utcnow()
+    """Create a tar gzipped archive of the directory web_static."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    dt = datetime.now()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        cur_time.year,
-        cur_time.month,
-        cur_time.day,
-        cur_time.hour,
-        cur_time.minute,
-        cur_time.second
+        dt.year,
+        dt.month,
+        dt.day,
+        dt.hour,
+        dt.minute,
+        dt.second
     )
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
-        return None
+    try:
+        print("Packing web_static to {}".format(file))
+        local("tar -cvzf {} web_static".format(file))
+        archize_size = os.stat(file).st_size
+        print("web_static packed: {} -> {} Bytes".format(file, archize_size))
+    except Exception:
+        file = None
     return file
