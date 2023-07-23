38f543e978f2b2d389ef966c9c20cb73838827e4
MokiEng <117774663+MokiEng@users.noreply.github.com>
2023-07-23 06:49:24 -0700

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,9 +4,10 @@ a script that starts a Flask web application:
 Routes:
     /cities_by_states: HTML page with a list of all states and related cities.
         H1 tag: “States”
-        UL tag: with the list of all State objects present in DBStorage sorted by name (A->Z)
-            LI tag: description of one State: <state.id>: <B><state.name></B> + UL tag:
-            with the list of City objects linked to the State sorted by name (A->Z)
+        UL tag: with the list of all State objects present in DBStorage
+        LI tag: description of one State: <state.id>:
+                <B><state.name></B> + UL tag:with the list of City objects
+                linked to the State sorted by name (A->Z)
               LI tag: description of one City: <city.id>: <B><city.name></B>
 """
 from models import storage
